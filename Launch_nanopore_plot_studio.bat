6ad8bab391edb838fd5589262d052d79d2ae24a8
SissiShi <85042375+SissiShi@users.noreply.github.com>
2026-04-20 20:55:05 -0400

    Update Launch_nanopore_plot_studio.bat

--- Launch_nanopore_plot_studio.bat
+++ Launch_nanopore_plot_studio.bat
@@ -1,22 +1,7 @@
 @echo off
 setlocal
 
-set "PYTHON_EXE=C:\Users\Odile\AppData\Local\Programs\Python\Python313\python.exe"
-set "SCRIPT_PATH=%~dp0nanopore_plot__studio_v11.py"
+set "SCRIPT_PATH=%~dp0nanopore_plot_studio.py"
 
-if not exist "%PYTHON_EXE%" (
-    echo Python not found:
-    echo %PYTHON_EXE%
-    pause
-    exit /b 1
-)
-
-if not exist "%SCRIPT_PATH%" (
-    echo Script not found:
-    echo %SCRIPT_PATH%
-    pause
-    exit /b 1
-)
-
-"%PYTHON_EXE%" "%SCRIPT_PATH%"
+py "%SCRIPT_PATH%"
 pause
